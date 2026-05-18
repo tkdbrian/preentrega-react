268e4f83876fb88c68d1c1c4c526ee8a1975e9ab
Brian <tkdbrian@gmail.com>
2026-05-17 23:01:28 -0300

    fix: precio simple

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -7,7 +7,7 @@ function Item({ name, description, price, image, stock, children }) {
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p className="card-description">{description}</p>
-        <p className="card-price">${price.toLocaleString('es-AR')}</p>
+        <p className="card-price">${price}</p>
         <p className="card-stock">Stock: {stock}</p>
         {children}
       </div>
