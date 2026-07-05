7ea7ae6172415c03d4fba0b968072e3a05837cfd
Brian <tkdbrian@gmail.com>
2026-07-04 23:28:56 -0300

    fix: restore Item CSS classes

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,13 +1,15 @@
 import "./Item.css";
 
-export const Item = ({ name, description, price, image, children }) => {
+export const Item = ({ name, description, price, image, stock, children }) => {
   return (
     <article className="card">
-      <img src={image} alt={`foto de ${name}`} />
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>${price}</p>
-      {children}
+      <img src={image} alt={`foto de ${name}`} className="card-img" />
+      <div className="card-body">
+        <h3 className="card-title">{name}</h3>
+        <p className="card-description">{description}</p>
+        <p className="card-price">${price}</p>
+        {children}
+      </div>
     </article>
   );
 };
